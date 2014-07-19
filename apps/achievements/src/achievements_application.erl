b9d9989e97edf4dbe0b3199eb7ec375283cce0f9
Caligin@Yuki <caligin35@yahoo.it>
2014-07-19 16:44:45 +0200

    enh: achievements app and sup

--- apps/achievements/src/achievements_application.erl
+++ apps/achievements/src/achievements_application.erl
@@ -1,1 +1,10 @@
-achievements_application.erl
+-module(achievements_application).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+   register(achievements_application, self()),
+   supervisor:start_link({local, achievements_supervisor}, achievements_supervisor, []).
+
+stop(_State) ->
+    ok.
--- apps/achievements/src/achievements_supervisor.erl
+++ apps/achievements/src/achievements_supervisor.erl
@@ -1,1 +1,13 @@
-achievements_supervisor.erl
+-module(achievements_supervisor).
+-behaviour(supervisor).
+-export([init/1]).
+-define(MAX_RESTART, 5).
+-define(MAX_TIME, 100).
+
+init([]) ->
+    {ok, {
+        { one_for_one, ?MAX_RESTART, ?MAX_TIME },
+        [
+            {achievements, {achievements, start_link, []}, permanent, brutal_kill, worker, [achievements]}
+        ]
+    }}.
