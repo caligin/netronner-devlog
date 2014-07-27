ce65935c18338c5d39f216ce2c30847a07e0d21c
Caligin@Theleth <caligin35@yahoo.it>
2014-07-28 01:44:17 +0200

    fix: parenthesis

--- netronner-webui/templatize.rb
+++ netronner-webui/templatize.rb
@@ -1,6 +1,6 @@
 base_path = "vars.properties"
 overrides_path = ENV["HOME"] + "/.vars.properties"
-source = File.file? overrides_path ? overrides_path : base_path 
+source = File.file?(overrides_path) ? overrides_path : base_path 
 
 File.open(source, "r") do |file|
     file.each_line do |line|
