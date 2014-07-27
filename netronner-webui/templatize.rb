dd757168a7b76e4e24b42bd17037823ce443521f
Caligin@Yuki <caligin35@yahoo.it>
2014-07-27 22:12:06 +0200

    enh: overrides mechanism for the templater

--- netronner-webui/templatize.rb
+++ netronner-webui/templatize.rb
@@ -1,4 +1,8 @@
-File.open("vars.properties", "r") do |file|
+base_path = "vars.properties"
+overrides_path = ENV["HOME"] + "/.vars.properties"
+source = File.file? overrides_path ? overrides_path : base_path 
+
+File.open(source, "r") do |file|
     file.each_line do |line|
          variable, value = line.strip.split(/\s*=\s*/)
         `sed -i 's|@@#{variable}@@|#{value}|g' target/site/index.html`
