6ae45a3ab10995f73a9e238068b5211383282039
Caligin@Theleth <caligin35@yahoo.it>
2014-07-28 20:21:01 +0200

    enh: delegating google auth complexity to google module

--- netronner-webapi/apps/authorization/src/google_token.erl
+++ netronner-webapi/apps/authorization/src/google_token.erl
@@ -5,47 +5,23 @@
 -export([is_authorized/1, principal/1]).
 
 is_authorized(Req) ->
-    { <<"Bearer ", AccessToken/binary>>, Req2} = cowboy_req:header(<<"authorization">>, Req), %% FIXME this bombs on a malformed token, want to return 401...
+    Token = access_token_from_req(Req),
     {ok, <<ClientId/binary>>} = application:get_env(google_api_client_id),
-    case validate_access_token(AccessToken, ClientId) of
+    case google:validate_access_token(Token, ClientId) of
         ok -> true;
         error -> {error, 401, [{<<"WWW-Authenticate">>, <<"Bearer error=\"invalid_token\"">>}]}
     end.
 
 principal(Req) ->
-    { <<"Bearer ", AccessToken/binary>>, _} = cowboy_req:header(<<"authorization">>, Req),
-    user_profile(AccessToken).
+    Token = access_token_from_req(Req),
+    GoogleUser = google:user_profile(Token, <<"me">>),
+    user_to_principal(GoogleUser).
 
-%% TODO consider using the key <<"expires_in">> => 3470 for caching control
--spec validate_access_token(AccessToken::binary(), ClientId::binary()) -> ok | error.
-validate_access_token(AccessToken, ClientId) ->
-    {ok, Response} = httpc:request("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" ++ binary_to_list(AccessToken)),
-    {{ _ProtoVersion, StatusCode, _StatusMessage }, _Headers, Body} = Response,
-    case StatusCode of
-        200 -> check_token_ownership(Body, ClientId);
-        400 -> error
-    end.
+access_token_from_req(Req) -> 
+    { <<"Bearer ", AccessToken/binary>>, _} = cowboy_req:header(<<"authorization">>, Req), %% FIXME this bombs on a malformed token, want to return 401...
+    google:access_token_make(AccessToken).
 
--spec check_token_ownership(BinaryTokenInfo::binary(), ClientId::binary()) -> ok | error.
-check_token_ownership(BinaryTokenInfo, ClientId) ->
-    TokenInfo = jiffy:decode(BinaryTokenInfo, [return_maps]),
-    case ClientId =:= maps:get(<<"issued_to">>, TokenInfo) of
-        true -> ok;
-        false -> error
-    end.
-
--spec user_profile(AccessToken::binary()) -> gen_auth:principal().
-user_profile(AccessToken) ->
-    {ok, Response} = httpc:request("https://www.googleapis.com/plus/v1/people/me?access_token=" ++ binary_to_list(AccessToken)),
-    {{ _ProtoVersion, 200, _StatusMessage }, _Headers, Body} = Response,
-    DecodedUserProfile = jiffy:decode(Body, [return_maps]),
-    to_principal(DecodedUserProfile).
-
--spec to_principal(#{binary() => term()}) -> principal:principal().
-to_principal(Decoded) ->
-    Id = maps:get(<<"id">>, Decoded),
-    Name = maps:get(<<"displayName">>, Decoded),
-    Image = maps:get(<<"url">>, maps:get(<<"image">>, Decoded)),
+user_to_principal(GoogleUser) ->
+    Id = google:user_id(GoogleUser),
+    Name = google:user_name(GoogleUser),
     principal:make(Id, Name).
-
-
